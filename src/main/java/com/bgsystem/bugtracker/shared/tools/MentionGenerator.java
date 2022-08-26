be23acd7c30178646a88c4d002a00959bb7c1ff5
Joseph <lievanosan@gmail.com>
2022-08-26 22:12:23 +0200

    Start the method generateMentions in the class MentionGenerator

---
+++ src/main/java/com/bgsystem/bugtracker/shared/tools/MentionGenerator.java
@@ -0,0 +1,17 @@
+package com.bgsystem.bugtracker.shared.tools;
+
+import com.bgsystem.bugtracker.models.client.project.bsPrMention.bsPrMentionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MentionGenerator {
+
+    @Autowired
+    private bsPrMentionRepository mentionRepository;
+
+    public void generateMentions(String comment){
+
+
+
+    }
+
+}
