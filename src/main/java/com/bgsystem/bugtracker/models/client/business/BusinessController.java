72175a7c2b2f0b496b0dca50712033327717c67e
Joseph <lievanosan@gmail.com>
2022-11-05 02:10:08 +0100

    Modify the BusinessController to serve the new BusinessListDTO with the method getAllForList

--- src/main/java/com/bgsystem/bugtracker/models/client/business/BusinessController.java
+++ src/main/java/com/bgsystem/bugtracker/models/client/business/BusinessController.java
@@ -2,15 +2,25 @@ package com.bgsystem.bugtracker.models.client.business;
 
 import com.bgsystem.bugtracker.shared.controller.DefaultController;
 import com.bgsystem.bugtracker.shared.service.DefaultService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/business")
 public class BusinessController extends DefaultController<BusinessDTO, BusinessMiniDTO, BusinessForm, Long> {
 
+    private BusinessServiceImplements service;
     protected BusinessController(BusinessServiceImplements service) {
         super(service);
+        this.service = service;
+    }
+
+    @GetMapping("/list")
+    public Collection<BusinessListDTO> getAllForList() {
+        return service.getAllForList();
     }
 
 }
