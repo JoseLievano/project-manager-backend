aece8b8038a95ed953ff362dd63643d84b6a3b3c
Joseph <lievanosan@gmail.com>
2023-01-14 01:56:48 +0100

    Add custom expressions in bsInvoicePredicate

--- src/main/java/com/bgsystem/bugtracker/models/client/bsInvoice/bsInvoicePredicate.java
+++ src/main/java/com/bgsystem/bugtracker/models/client/bsInvoice/bsInvoicePredicate.java
@@ -33,9 +33,49 @@ public class bsInvoicePredicate extends CommonPathExpression<bsInvoiceEntity> {
             case "client" -> getClientExpression(filter);
             case "project" -> getProjectExpression(filter);
             case "business" -> getBusinessExpression(filter);
+            case "task" -> getTaskExpression(filter);
             default -> throw new IllegalArgumentException("Invalid field: " + filter.getField());
         };
 
+    }
+
+    private BooleanExpression getTaskExpression(FilterRequest filter) throws BadOperator {
+
+        BooleanExpression taskExpression = null;
+
+        for (FilterOperator operation : filter.getOperations()){
+
+            switch (operation.getField()){
+                case "id" -> {
+                    NumberPath<Long> idPath = bsInvoiceEntity.task.id;
+                    taskExpression = addOrExpression(taskExpression, getNumberPathBooleanExpression(idPath, operation));
+                }
+                case "name" -> {
+                    StringPath namePath = bsInvoiceEntity.task.name;
+                    taskExpression = addOrExpression(taskExpression, getStringPathBooleanExpression(namePath, operation));
+                }
+                case "dueDate" -> {
+                    DatePath<Date> dueDatePath = entityPath.getDate(bsInvoiceEntity.task.dueDate.toString(), java.util.Date.class);
+                    taskExpression = addOrExpression(taskExpression, getDatePathBooleanExpression(dueDatePath, operation));
+                }
+                case "isInternal" -> {
+                    BooleanPath isInternalPath = bsInvoiceEntity.task.isInternal;
+                    taskExpression = addOrExpression(taskExpression, getBooleanPathBooleanExpression(isInternalPath, operation));
+                }
+                case "isOverDue" -> {
+                    BooleanPath isOverDuePath = bsInvoiceEntity.task.isOverDue;
+                    taskExpression = addOrExpression(taskExpression, getBooleanPathBooleanExpression(isOverDuePath, operation));
+                }
+                case "isDone" -> {
+                    BooleanPath isDonePath = bsInvoiceEntity.task.isDone;
+                    taskExpression = addOrExpression(taskExpression, getBooleanPathBooleanExpression(isDonePath, operation));
+                }
+                default -> throw new IllegalArgumentException("Invalid field: " + operation.getField());
+            }
+
+        }
+
+        return taskExpression;
     }
 
     private BooleanExpression getBusinessExpression(FilterRequest filter) throws BadOperator{
@@ -45,7 +85,6 @@ public class bsInvoicePredicate extends CommonPathExpression<bsInvoiceEntity> {
         for (FilterOperator operation : filter.getOperations()){
 
             switch (operation.getField()){
-
                 case "id" -> {
                     NumberPath<Long> idPath = bsInvoiceEntity.business.id;
                     businessExpression = addOrExpression(businessExpression, getNumberPathBooleanExpression(idPath, operation));
@@ -71,7 +110,6 @@ public class bsInvoicePredicate extends CommonPathExpression<bsInvoiceEntity> {
                     businessExpression = addOrExpression(businessExpression, getBooleanPathBooleanExpression(isActivePath, operation));
                 }
                 default -> throw new IllegalArgumentException("Field is invalid: " + operation.getField());
-
             }
 
         }
@@ -87,7 +125,6 @@ public class bsInvoicePredicate extends CommonPathExpression<bsInvoiceEntity> {
         for (FilterOperator operation : filter.getOperations()){
 
             switch (operation.getField()){
-
                 case "id" -> {
                     NumberPath<Long> idPath = bsInvoiceEntity.project.id;
                     projectExpression = addOrExpression(projectExpression, getNumberPathBooleanExpression(idPath, operation));
@@ -109,7 +146,6 @@ public class bsInvoicePredicate extends CommonPathExpression<bsInvoiceEntity> {
                     projectExpression = addOrExpression(projectExpression, getDatePathBooleanExpression(dueDatePath, operation));
                 }
                 default -> throw new IllegalArgumentException("Invalid field: " + operation.getField());
-
             }
 
         }
@@ -123,7 +159,6 @@ public class bsInvoicePredicate extends CommonPathExpression<bsInvoiceEntity> {
         BooleanExpression clientExpression = null;
 
         for (FilterOperator operation : filter.getOperations()) {
-
             switch (operation.getField()) {
                 case "id" -> {
                     NumberPath<Long> numberPath = bsInvoiceEntity.client.id;
