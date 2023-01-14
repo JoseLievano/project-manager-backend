e80f0a15d09896b2741056e0ae81ce7579d107fa
Joseph <lievanosan@gmail.com>
2023-01-14 02:10:30 +0100

    Add custom expressions in bsKBCategoryPredicate and bsKBPredicate

--- src/main/java/com/bgsystem/bugtracker/models/client/bsKB/bsKBPredicate.java
+++ src/main/java/com/bgsystem/bugtracker/models/client/bsKB/bsKBPredicate.java
@@ -1,7 +1,10 @@
 package com.bgsystem.bugtracker.models.client.bsKB;
 
+import com.bgsystem.bugtracker.exeptions.BadOperator;
 import com.bgsystem.bugtracker.shared.models.listRequest.CommonPathExpression;
-import com.querydsl.core.types.dsl.PathBuilder;
+import com.bgsystem.bugtracker.shared.models.listRequest.FilterOperator;
+import com.bgsystem.bugtracker.shared.models.listRequest.FilterRequest;
+import com.querydsl.core.types.dsl.*;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +15,77 @@ public class bsKBPredicate extends CommonPathExpression<bsKBEntity> {
     public bsKBPredicate(){
         super( );
         this.entityPath = new PathBuilder<bsKBEntity>(bsKBEntity.class, "bsKBEntity");
+        this.entityFields.add("bsKBCategory");
+        this.entityFields.add("business");
     }
 
+    @Override
+    protected BooleanExpression getCustomPathExpression(FilterRequest filter) throws BadOperator {
+        return switch (filter.getField()) {
+            case "bsKBCategory" -> getbsKBCategoryExpression(filter);
+            case "business" -> getBusinessExpression(filter);
+            default -> throw new IllegalArgumentException("Invalid field: " + filter.getField());
+        };
+    }
+
+    private BooleanExpression getbsKBCategoryExpression(FilterRequest filter) throws BadOperator {
+
+        BooleanExpression bsKBCategoryExpression = null;
+
+        for (FilterOperator operation : filter.getOperations()){
+
+            switch (operation.getField()) {
+                case "id" -> {
+                    NumberPath<Long> idPath = bsKBEntity.bsKBCategory.id;
+                    bsKBCategoryExpression = addOrExpression(bsKBCategoryExpression, getNumberPathBooleanExpression(idPath, operation));
+                }
+                case "name" -> {
+                    StringPath namePath = bsKBEntity.bsKBCategory.name;
+                    bsKBCategoryExpression = addOrExpression(bsKBCategoryExpression, getStringPathBooleanExpression(namePath, operation));
+                }
+                default -> throw new IllegalArgumentException("Invalid field: " + operation.getField());
+            }
+        }
+
+        return bsKBCategoryExpression;
+
+    }
+
+    private BooleanExpression getBusinessExpression(FilterRequest filter) throws BadOperator{
+
+        BooleanExpression businessExpression = null;
+
+        for (FilterOperator operation : filter.getOperations()){
+            switch (operation.getField()){
+                case "id" -> {
+                    NumberPath<Long> idPath = bsKBEntity.business.id;
+                    businessExpression = addOrExpression(businessExpression, getNumberPathBooleanExpression(idPath, operation));
+                }
+                case "name" -> {
+                    StringPath namePath = bsKBEntity.business.name;
+                    businessExpression = addOrExpression(businessExpression, getStringPathBooleanExpression(namePath, operation));
+                }
+                case "taxID" -> {
+                    StringPath taxIDPath = bsKBEntity.business.taxID;
+                    businessExpression = addOrExpression(businessExpression, getStringPathBooleanExpression(taxIDPath, operation));
+                }
+                case "pendingInvoice" -> {
+                    BooleanPath pendingInvoicePath = bsKBEntity.business.pendingInvoice;
+                    businessExpression = addOrExpression(businessExpression, getBooleanPathBooleanExpression(pendingInvoicePath, operation));
+                }
+                case "overDue" -> {
+                    BooleanPath overDuePath = bsKBEntity.business.overDue;
+                    businessExpression = addOrExpression(businessExpression, getBooleanPathBooleanExpression(overDuePath, operation));
+                }
+                case "isActive" -> {
+                    BooleanPath isActivePath = bsKBEntity.business.isActive;
+                    businessExpression = addOrExpression(businessExpression, getBooleanPathBooleanExpression(isActivePath, operation));
+                }
+                default -> throw new IllegalArgumentException("Invalid field: " + operation.getField());
+            }
+        }
+
+        return businessExpression;
+
+    }
 }
--- src/main/java/com/bgsystem/bugtracker/models/client/bsKBCategory/bsKBCategoryPredicate.java
+++ src/main/java/com/bgsystem/bugtracker/models/client/bsKBCategory/bsKBCategoryPredicate.java
@@ -1,7 +1,10 @@
 package com.bgsystem.bugtracker.models.client.bsKBCategory;
 
+import com.bgsystem.bugtracker.exeptions.BadOperator;
 import com.bgsystem.bugtracker.shared.models.listRequest.CommonPathExpression;
-import com.querydsl.core.types.dsl.PathBuilder;
+import com.bgsystem.bugtracker.shared.models.listRequest.FilterOperator;
+import com.bgsystem.bugtracker.shared.models.listRequest.FilterRequest;
+import com.querydsl.core.types.dsl.*;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +15,77 @@ public class bsKBCategoryPredicate extends CommonPathExpression<bsKBCategoryEnti
     public bsKBCategoryPredicate(){
         super( );
         this.entityPath = new PathBuilder<bsKBCategoryEntity>(bsKBCategoryEntity.class, "bsKBCategoryEntity");
+        this.entityFields.add("business");
+        this.entityFields.add("bsKB");
     }
 
+    @Override
+    protected BooleanExpression getCustomPathExpression(FilterRequest filter) throws BadOperator {
+        return switch (filter.getField()) {
+            case "business" -> getBusinessExpression(filter);
+            case "bsKB" -> getbsKBExpression(filter);
+            default -> throw new IllegalArgumentException("Invalid field: " + filter.getField());
+        };
+    }
+
+    private BooleanExpression getbsKBExpression(FilterRequest filter) throws BadOperator {
+
+        BooleanExpression bsKBExpression = null;
+
+        for (FilterOperator operation : filter.getOperations()){
+            switch (operation.getField()){
+                case "id" -> {
+                    NumberPath<Long> idPath = bsKBCategoryEntity.bsKBEntities.any().id;
+                    bsKBExpression = addOrExpression(bsKBExpression, getNumberPathBooleanExpression(idPath, operation));
+                }
+                case "title" -> {
+                    StringPath titlePath = bsKBCategoryEntity.bsKBEntities.any().title;
+                    bsKBExpression = addOrExpression(bsKBExpression, getStringPathBooleanExpression(titlePath, operation));
+                }
+                default -> throw new IllegalArgumentException("Invalid field: " + operation.getField());
+            }
+        }
+
+        return bsKBExpression;
+
+    }
+
+    private BooleanExpression getBusinessExpression(FilterRequest filter) throws BadOperator {
+
+        BooleanExpression businessExpression = null;
+
+        for (FilterOperator operation : filter.getOperations()){
+
+            switch (operation.getField()) {
+                case "id" -> {
+                    NumberPath<Long> idPath = bsKBCategoryEntity.business.id;
+                    businessExpression = addOrExpression(businessExpression, getNumberPathBooleanExpression(idPath, operation));
+                }
+                case "name" -> {
+                    StringPath namePath = bsKBCategoryEntity.business.name;
+                    businessExpression = addOrExpression(businessExpression, getStringPathBooleanExpression(namePath, operation));
+                }
+                case "taxID" -> {
+                    StringPath taxIDPath = bsKBCategoryEntity.business.taxID;
+                    businessExpression = addOrExpression(businessExpression, getStringPathBooleanExpression(taxIDPath, operation));
+                }
+                case "pendingInvoice" ->{
+                    BooleanPath pendingInvoicePath = bsKBCategoryEntity.business.pendingInvoice;
+                    businessExpression = addOrExpression(businessExpression, getBooleanPathBooleanExpression(pendingInvoicePath, operation));
+                }
+                case "overDue" -> {
+                    BooleanPath overDuePath = bsKBCategoryEntity.business.overDue;
+                    businessExpression = addOrExpression(businessExpression, getBooleanPathBooleanExpression(overDuePath, operation));
+                }
+                case "isActive" -> {
+                    BooleanPath isActivePath = bsKBCategoryEntity.business.isActive;
+                    businessExpression = addOrExpression(businessExpression, getBooleanPathBooleanExpression(isActivePath, operation));
+                }
+                default -> throw new IllegalArgumentException("Invalid field: " + operation.getField());
+            }
+        }
+
+        return businessExpression;
+
+    }
 }
