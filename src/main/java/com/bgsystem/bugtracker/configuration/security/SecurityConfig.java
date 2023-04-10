56c01d8e39764a81884776b5c697dfb6b29dfa00
Joseph <lievanosan@gmail.com>
2023-04-10 17:29:23 +0200

    Update SecurityConfig with improvements for readability and maintainability
    This commit updates the SecurityConfig class by applying the following changes:
    
    Replace constructor with @Autowired annotation by Lombok's @RequiredArgsConstructor
    Extract CORS configuration to a separate method for better readability
    Add @EnableGlobalMethodSecurity annotation with prePostEnabled set to true

--- src/main/java/com/bgsystem/bugtracker/configuration/security/SecurityConfig.java
+++ src/main/java/com/bgsystem/bugtracker/configuration/security/SecurityConfig.java
@@ -4,10 +4,11 @@ import com.bgsystem.bugtracker.configuration.filter.JWTTokenGeneratorFilter;
 import com.bgsystem.bugtracker.configuration.filter.JWTTokenValidatorFilter;
 import com.bgsystem.bugtracker.shared.models.securityUser.SecurityUserServiceImplements;
 import com.bgsystem.bugtracker.shared.models.user.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,40 +22,37 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final UserRepository userRepository;
-
     private final SecurityUserServiceImplements securityUserServiceImplements;
-
-    @Autowired
-    public SecurityConfig(UserRepository userRepository, SecurityUserServiceImplements securityUserServiceImplements) {
-        this.userRepository = userRepository;
-        this.securityUserServiceImplements = securityUserServiceImplements;
-    }
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-
         http
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .cors().configurationSource(request -> {
-                    CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-                    config.setAllowedMethods(Collections.singletonList("*"));
-                    config.setAllowCredentials(true);
-                    config.setAllowedHeaders(Collections.singletonList("*"));
-                    config.setExposedHeaders(List.of("Authorization"));
-                    config.setMaxAge(3600L);
-                    return config;
-                }).and().csrf().disable()
+                .cors().configurationSource(request -> corsConfiguration())
+                .and().csrf().disable()
                 .addFilterBefore(new JWTTokenValidatorFilter(securityUserServiceImplements), BasicAuthenticationFilter.class)
                 .addFilterAfter(new JWTTokenGeneratorFilter(userRepository), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests((auth) -> auth
                         .anyRequest().authenticated()
-        ).httpBasic(Customizer.withDefaults());
+                ).httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    private CorsConfiguration corsConfiguration() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setExposedHeaders(List.of("Authorization"));
+        config.setMaxAge(3600L);
+        return config;
     }
 
     @Bean
