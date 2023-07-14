910260d07c694e9289c2998ab5e30a001050238c
Joseph <lievanosan@gmail.com>
2023-07-14 13:57:23 +0200

    Override update and delete method.
    Creates the new updateOrder method.

--- src/main/java/com/bgsystem/bugtracker/models/client/bsPriority/bsPriorityServiceImplements.java
+++ src/main/java/com/bgsystem/bugtracker/models/client/bsPriority/bsPriorityServiceImplements.java
@@ -2,12 +2,18 @@ package com.bgsystem.bugtracker.models.client.bsPriority;
 
 import com.bgsystem.bugtracker.exeptions.ElementAlreadyExist;
 import com.bgsystem.bugtracker.exeptions.ElementNotFoundException;
+import com.bgsystem.bugtracker.exeptions.InvalidDeleteOperation;
 import com.bgsystem.bugtracker.exeptions.InvalidInsertDeails;
 import com.bgsystem.bugtracker.models.client.business.BusinessEntity;
 import com.bgsystem.bugtracker.models.client.business.BusinessRepository;
 import com.bgsystem.bugtracker.shared.service.DefaultServiceImplements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class bsPriorityServiceImplements extends DefaultServiceImplements <bsPriorityDTO, bsPriorityMiniDTO, bsPriorityListDTO, bsPriorityForm, bsPriorityEntity, Long> {
@@ -40,12 +46,10 @@ public class bsPriorityServiceImplements extends DefaultServiceImplements <bsPri
 
         //Check if a priority with the same name already exists
         if ( priorityRepository.findByName(bsPriorityForm.getName()).size() > 0 ) {
-            throw new ElementAlreadyExist("Priority already exists");
+            throw new ElementAlreadyExist("Priority with that name already exists");
         }
 
         bsPriorityEntity toInsert = mapper.toEntity(bsPriorityForm);
-
-        System.out.println("business id: " + bsPriorityForm.getBusiness());
 
         //Get the business
         BusinessEntity business = businessRepository.findById(bsPriorityForm.getBusiness()).orElseThrow(() -> new ElementNotFoundException("Business not found"));
@@ -59,10 +63,74 @@ public class bsPriorityServiceImplements extends DefaultServiceImplements <bsPri
         //Save the business
         businessRepository.save(business);
 
+        //Get how many priorities business already has
+        long amountOfPriorities = (long) business.getBsPriorities().size();
+        toInsert.setPriorityOrder((int) amountOfPriorities);
+
         //Save the priority
         repository.save(toInsert);
 
         return mapper.toSmallDTO(toInsert);
+
+    }
+
+    @Override
+    public bsPriorityDTO update(Long id, bsPriorityForm form) throws ElementNotFoundException, InvalidInsertDeails {
+
+        if (form == null)
+            throw new InvalidInsertDeails();
+
+        bsPriorityEntity toEdit = repository.findById(id).orElseThrow(ElementNotFoundException::new);
+
+        if (form.getName() != null)
+            toEdit.setName(form.getName());
+
+
+        if (form.getPriorityOrder() != null)
+            toEdit.setPriorityOrder(form.getPriorityOrder());
+
+        repository.save(toEdit);
+
+        return mapper.toDTO(toEdit);
+    }
+
+    public Set<bsPriorityListDTO> updateOrder (Set<bsPriorityForm> formPriorities) throws ElementNotFoundException{
+        Set<bsPriorityEntity> toEditPriorities = new HashSet<>();
+
+        for (bsPriorityForm formPriority : formPriorities) {
+            bsPriorityEntity toEdit = repository.findById(formPriority.getId()).orElseThrow(ElementNotFoundException::new);
+            toEdit.setPriorityOrder(formPriority.getPriorityOrder());
+            toEditPriorities.add(toEdit);
+        }
+
+        repository.saveAll(toEditPriorities);
+
+        return toEditPriorities.stream()
+                .map(mapper::toListDTO)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public bsPriorityDTO delete(Long id) throws ElementNotFoundException, InvalidDeleteOperation {
+
+        if (id == null)
+            throw new InvalidDeleteOperation("Invalid priority ID");
+
+        bsPriorityEntity toDelete = repository.findById(id).orElseThrow(ElementNotFoundException::new);
+
+        Integer toDelPriorityOrder = toDelete.getPriorityOrder();
+
+        ArrayList<bsPriorityEntity> prioritiesToModify = priorityRepository.findByPriorityOrderGreaterThan(toDelPriorityOrder);
+
+        for (bsPriorityEntity priority : prioritiesToModify){
+            priority.setPriorityOrder(priority.getPriorityOrder() - 1);
+        }
+
+        repository.saveAll(prioritiesToModify);
+
+        repository.delete(toDelete);
+
+        return mapper.toDTO(toDelete);
 
     }
 
