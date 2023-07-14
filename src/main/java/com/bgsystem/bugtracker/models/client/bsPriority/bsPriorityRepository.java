9e05749dc718594e7327ab88e90f7d6187256669
Joseph <lievanosan@gmail.com>
2023-07-14 13:56:36 +0200

    Add new method to find bsPriorities with a greater priority order.

--- src/main/java/com/bgsystem/bugtracker/models/client/bsPriority/bsPriorityRepository.java
+++ src/main/java/com/bgsystem/bugtracker/models/client/bsPriority/bsPriorityRepository.java
@@ -1,14 +1,16 @@
 package com.bgsystem.bugtracker.models.client.bsPriority;
 
 import com.bgsystem.bugtracker.shared.repository.DefaultRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 @Service
 public interface bsPriorityRepository extends DefaultRepository<bsPriorityEntity, Long> {
 
     Set<bsPriorityEntity> findByName (String name);
+
+    ArrayList<bsPriorityEntity> findByPriorityOrderGreaterThan(Integer priorityOrderToDelete);
 
 }
