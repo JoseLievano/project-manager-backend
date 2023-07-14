af3f83ab736d3a830ab7887bb77773af1397f238
Joseph <lievanosan@gmail.com>
2023-07-14 13:59:21 +0200

    Add new "/update-order" end point.

--- src/main/java/com/bgsystem/bugtracker/models/client/bsPriority/bsPriorityController.java
+++ src/main/java/com/bgsystem/bugtracker/models/client/bsPriority/bsPriorityController.java
@@ -1,15 +1,29 @@
 package com.bgsystem.bugtracker.models.client.bsPriority;
 
+import com.bgsystem.bugtracker.exeptions.ElementNotFoundException;
 import com.bgsystem.bugtracker.shared.controller.DefaultController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/bs_priority")
 public class bsPriorityController extends DefaultController <bsPriorityDTO, bsPriorityMiniDTO, bsPriorityListDTO, bsPriorityForm, Long> {
 
+    private final bsPriorityServiceImplements bsPriorityService;
+
     public bsPriorityController(bsPriorityServiceImplements service) {
         super(service);
+        this.bsPriorityService = service;
+    }
+
+    @PutMapping("/update-order")
+    public ResponseEntity<Set<bsPriorityListDTO>> updateOrder (@RequestBody Set<bsPriorityForm> forms) throws ElementNotFoundException {
+        return ResponseEntity.ok(bsPriorityService.updateOrder(forms));
     }
 
 }
