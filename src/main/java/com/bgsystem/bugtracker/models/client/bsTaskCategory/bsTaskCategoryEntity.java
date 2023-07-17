cac16daf0d66e0d60a0622326d42658c9529c5c5
Joseph <lievanosan@gmail.com>
2023-07-17 11:49:42 +0200

    Add types in a ManyToMany relationship.

--- src/main/java/com/bgsystem/bugtracker/models/client/bsTaskCategory/bsTaskCategoryEntity.java
+++ src/main/java/com/bgsystem/bugtracker/models/client/bsTaskCategory/bsTaskCategoryEntity.java
@@ -1,5 +1,6 @@
 package com.bgsystem.bugtracker.models.client.bsTaskCategory;
 
+import com.bgsystem.bugtracker.models.client.bsType.bsTypeEntity;
 import com.bgsystem.bugtracker.models.client.business.BusinessEntity;
 import com.bgsystem.bugtracker.models.client.project.bsPrTask.bsPrTaskEntity;
 import lombok.*;
@@ -26,6 +27,17 @@ public class bsTaskCategoryEntity {
     @ManyToOne(optional = false)
     @JoinColumn(name = "business_entity_id", nullable = false)
     private BusinessEntity business;
+
+    @ManyToMany (fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "task_category_and_type_relations",
+            joinColumns = @JoinColumn(name = "task_id"),
+            inverseJoinColumns = @JoinColumn(name = "type_id")
+    )
+    private Set<bsTypeEntity> types;
+
+    @Column
+    private Long typesCount;
 
     @OneToMany(mappedBy = "category")
     private Set<bsPrTaskEntity> tasks;
